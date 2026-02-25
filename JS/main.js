58b1a6f068d3953c5e7d0e29a6e9a474ee422ffd
jeremy_intriago <jeremy.secundaria@gmail.com>
2026-02-25 19:03:27 +0100

    Ajustes

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
-(() => {
-'use strict'
+
 
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
 const forms = document.querySelectorAll('.needs-validation')
@@ -15,20 +14,26 @@ Array.from(forms).forEach(form => {
     form.classList.add('was-validated')
     }, false)
 })
-})()
 
 const navbar = document.getElementById("navbar");
 const navbar_brand = document.getElementById("navbar-brand");
+const offcanvas_menu = document.getElementById("offcanvas-menu");
 const listener = document.addEventListener('scroll', e =>{
     if (Number(window.pageYOffset) != 0) {
         navbar.classList.add("is-sticky");
         navbar.classList.add("bg-body-tertiary")
+
+        offcanvas_menu.classList.remove("text-white");
+        offcanvas_menu.classList.add('text-black');
+
         navbar_brand.src = "./IMG/owp-mentor-logo-b.png";
     }else if(Number(window.pageYOffset) == 0){
         navbar.classList.remove("is-sticky");
         navbar.classList.remove("bg-body-tertiary")
-        navbar.classList.add("text-white")
-        navbar_brand.src = "./IMG/owp-mentor-logo-w.png";
+
+        offcanvas_menu.classList.remove("text-black");
+        offcanvas_menu.classList.add('text-white');
         
+        navbar_brand.src = "./IMG/owp-mentor-logo-w.png";
     }
 })
